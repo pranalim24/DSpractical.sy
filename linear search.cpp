27ade0204c1589d240dd856a8af29ef43f025a71
pranalim24 <mohitepranali05@gmail.com>
2026-09-25 12:33:29 +0530

    Add files via upload

---
+++ linear search.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[100], n, key, i;
+    
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    cout << "Enter elements:\n";
+    for(i = 0; i < n; i++)
+        cin >> a[i];
+
+    cout << "Enter element to search: ";
+    cin >> key;
+
+    for(i = 0; i < n; i++)
+    {
+        if(a[i] == key)
+        {
+            cout << "Element found at position " << i + 1;
+            return 0;
+        }
+    }
+
+    cout << "Element not found";
+
+    return 0;
+}
+
